ad7958fcabb4156ccf5ca2fce499e228b7baf913
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-05-03 14:32:50 +0200

    firsttask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function randomColor () {
+document.body.style.backgroundColor = getRandomHexColor();
+
+  }
+
+  function changeColor () {
+    startBtn.disabled = true;
+timerId = setInterval(randomColor,1000);
+  }
+
+function stopChangeColor () {
+    clearInterval(timerId);
+    startBtn.disabled=false;
+}
+
+startBtn.addEventListener('click',changeColor);
+stopBtn.addEventListener('click',stopChangeColor)
