3c29d69279e34a9612c3c33d09f1d03cc653f07b
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-05-06 10:05:15 +0000

    Deploying to gh-pages from @ MikolajLesniczuk/goit-js-hw-09@6c79e6b9871c72693cc494d4c3041afcdb7e077f 🚀

---
+++ 01-color-switcher.e204441f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0,t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,n(),d=setInterval(n,1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.e204441f.js.map
